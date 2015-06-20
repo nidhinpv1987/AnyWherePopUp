8535f1947609c83865d12adfd2759ab9d325310e
Nidhin p.v <nidhinpv1987@gmail.com>
2015-06-20 18:14:49 +0530

    UI updates

--- app/src/main/java/com/popup/anywherepopup/MainActivity.java
+++ app/src/main/java/com/popup/anywherepopup/MainActivity.java
@@ -1,10 +1,15 @@
 package com.popup.anywherepopup;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.anywherepopup.view.AnimationStyle;
 import com.anywherepopup.view.AnyWherePopUp;
@@ -12,44 +17,53 @@ import com.anywherepopup.view.AnyWherePopUp;
 
 public class MainActivity extends ActionBarActivity {
     private int i = 0;
+    private TextView text;
     private AnyWherePopUp pop;
-    int[] anims = {AnimationStyle.FADE,
+    int[] anims = {
+            AnimationStyle.FADE,
             AnimationStyle.SLIDE_TOP_IN_TOP_OUT,
             AnimationStyle.SLIDE_BOTTOM_IN_BOTTOM_OUT,
             AnimationStyle.SLIDE_RIGHT_IN_RIGHT_OUT,
             AnimationStyle.SLIDE_LEFT_IN_LEFT_OUT,
             AnimationStyle.SLIDE_RIGHT_IN_LEFT_OUT,
+            AnimationStyle.SLIDE_LEFT_IN_RIGHT_OUT,
             AnimationStyle.SLIDE_BOTTOM_IN_TOP_OUT,
             AnimationStyle.SLIDE_TOP_IN_BOTTOM_OUT,
-            AnimationStyle.SLIDE_LEFT_IN_RIGHT_OUT};
+    };
+    String animName[]={"AnimationStyle.FADE",
+            "AnimationStyle.SLIDE_TOP_IN_TOP_OUT",
+            "AnimationStyle.SLIDE_BOTTOM_IN_BOTTOM_OUT",
+            "AnimationStyle.SLIDE_RIGHT_IN_RIGHT_OUT",
+            "AnimationStyle.SLIDE_LEFT_IN_LEFT_OUT",
+            "AnimationStyle.SLIDE_RIGHT_IN_LEFT_OUT",
+            "AnimationStyle.SLIDE_LEFT_IN_RIGHT_OUT",
+            "AnimationStyle.SLIDE_BOTTOM_IN_TOP_OUT",
+            "AnimationStyle.SLIDE_TOP_IN_BOTTOM_OUT"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pop = new AnyWherePopUp(MainActivity.this);
-
-
+        text = (TextView) findViewById(R.id.show);
+        text.setText(animName[0]);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
         return true;
     }
 
     public void toggleAnimation(View v) {
-
-        try {
-            pop.setAnimation(anims[i]);
-            pop.show(v, R.layout.layout);
-            i++;
-        } catch (Exception e) {
+        if (i == 9)
             i = 0;
-            e.printStackTrace();
-        }
+        text.setText(animName[i]);
+        pop.setAnimation(anims[i]);
+        pop.show(v, R.layout.layout);
+        i++;
+
     }
 
     @Override
